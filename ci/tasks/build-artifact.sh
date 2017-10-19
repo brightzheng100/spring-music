580a1e18d6d6f505e410caaad5ad974316b85a0a
Bright Zheng <bright.zheng@outlook.com>
2017-10-19 17:49:43 +0800

    fixed issues in pipeline-simple

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -5,6 +5,6 @@ export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 echo "version to use: ${version}"
 
-cd git-repo
-gradle assemble -Pversion=$version
+cd resource-git
+./gradlew assemble -Pversion=$version
 cp build/libs/*.jar ../artifact-dir/
