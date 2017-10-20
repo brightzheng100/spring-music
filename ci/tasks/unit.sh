51427bf0ea3317813b916e06bb8011a29c6915ec
Bright Zheng <bright.zheng@outlook.com>
2017-10-20 13:54:29 +0800

    tuned the complete pipeline

--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+export ROOT_FOLDER=$( pwd )
+export GRADLE_USER_HOME="${ROOT_FOLDER}/.gradle"
 export GRADLE_OPTS=-Dorg.gradle.native=false
-cd git-repo
+
+cd resource-git
+
 gradle -v
 gradle test
--- ci/tasks/verify-test.sh
+++ ci/tasks/verify-test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-get_code="curl -I $CONCOURSE_DEMO_URL 2>/dev/null | head -n 1 | cut -d$' ' -f2"
+get_code="curl -I ${concourse_demo_url} 2>/dev/null | head -n 1 | cut -d$' ' -f2"
 status_code=`eval $get_code`
 echo "The status code returned is: $status_code"
 
 if [ "$status_code" != "200" ]
 then
-  echo "Expect status code from $CONCOURSE_DEMO_URL as 200, but got $status_code"
+  echo "Expected status code from ${concourse_demo_url} is 200, but got $status_code"
   exit 1
 fi
